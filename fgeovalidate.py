78af570b298830a8954fcafebb841cdbf4995bb3
mrajerisonCerema <mathieu.rajerison@cerema.fr>
2022-01-18 10:10:09 +0100

    colonnes géométriques à vérifier, champ getGeoField

--- fgeovalidate.py
+++ fgeovalidate.py
@@ -16,11 +16,14 @@ def readSchema(filePath):
     schema = json.load(f)
     return(schema)
     
-def getGeoFields(schema):
+def getGeoFields(schema, index = 0):
     geoFields = [elt for elt in schema['fields'] if elt['type'] == 'wkt']
-    geoFields = geoFields[0]
     return(geoFields)
     
+def getGeoField(schema, index = 0):
+    geoFields = getGeoFields(schema, index)
+    geoField = geoFields[index]
+    return(geoField)
 
 # Bounds
 def controlXmin(xmin, refXmin) :
@@ -139,7 +142,7 @@ def getGeomFromWkt(wkt):
     geom = gpd.GeoSeries.from_wkt([wkt])
     return(geom)
     
-def readData(filePath, geomCol = '_geom'):
+def readData(filePath, schema, geomCol = None):
     print('File : %s\n'%filePath)
     
     fileExtension = pathlib.Path(filePath).suffix
@@ -150,9 +153,24 @@ def readData(filePath, geomCol = '_geom'):
         data = pd.read_csv(filePath)
         nRows = data.shape[0]
         
+        # Get geometry column from schema
+        if geomCol is None:
+            geoFields = getGeoFields(schema)
+            geomCol = getGeoField(schema)['name']
+            if len(geoFields) == 1:
+                print("Geometry column : '%s' is the geometry column, according to the schema\n"%geomCol)
+            else :
+                print("%d geometry columns are present in the schema."%(len(geoFields)))
+                print("Only %s will be analysed.\n"%(geomCol))
+                print("You can choose the geometry column that will be controlled with :")
+                print("> geovalidate data.csv schema.json wktcolumn")
+                print("")
+        
         # Check if geometry column exists
         if not controlGeomColExists(data, geomCol):
             sys.exit("[Error] '%s' geometry column does not exist. Exiting..."%geomCol)
+        else:
+            print("Analyzing %s...\n"%geomCol)
         
         # Get rows without geometries
         emptyGeomRows = getEmptyGeomRows(data, geomCol)
@@ -196,26 +214,28 @@ def readData(filePath, geomCol = '_geom'):
     return(res)
         
 # Process
-def geovalidate(dataPath, schemaPath, geomCol='_geom'):
+def geovalidate(dataPath, schemaPath, geomCol = None):
     
     # Schema
     if not os.path.exists(schemaPath):
         sys.exit('[Error] %s does not exist. Exiting...'%schemaPath)
     
     schema = readSchema(schemaPath)
-    geoFields = getGeoFields(schema)
+    geoField = getGeoField(schema)
     
     # Data
     if not os.path.exists(dataPath):
         sys.exit('[Error] %s does not exist. Exiting...'%dataPath)
     
-    res = readData(dataPath, geomCol)
+    # Read Data
+    res = readData(dataPath, schema, geomCol)
     data = res['data']
     mapping = res['mapping']
     
     # Control
     for elt in mapping:
         
+        # NULL GEOMETRIES
         if mapping[elt] is None:
             print('%d : the row does not have a geometry'%elt)
         else:
@@ -223,24 +243,28 @@ def geovalidate(dataPath, schemaPath, geomCol='_geom'):
         
             geom = data.geometry[i]
             
-            if 'crs' in geoFields.keys():
-                okCRS = controlCRS(data.crs.srs, geoFields['crs'])
-                if not okCRS:
-                    print('%d : wrong %s CRS. Must be %s'%(i, data.crs.srs, geoFields['crs']))
+            # ~ # CRS
+            # ~ if 'crs' in geoField.keys():
+                # ~ okCRS = controlCRS(data.crs.srs, geoField['crs'])
+                # ~ if not okCRS:
+                    # ~ print('%d : wrong %s CRS. Must be %s'%(i, data.crs.srs, geoField['crs']))
             
-            if 'geomtype' in geoFields.keys(): 
-                okGeomType = controlGeomType(geom.geom_type, geoFields['geomtype'])
+            # GEOMTYPE
+            if 'geomtype' in geoField.keys(): 
+                okGeomType = controlGeomType(geom.geom_type, geoField['geomtype'])
                 if not okGeomType:
-                    print("%d : '%s' is found but '%s' geometry type is required"%(i, geom.geom_type, geoFields['geomtype']))
-                
+                    print("%d : '%s' is found but '%s' geometry type is required"%(i, geom.geom_type, geoField['geomtype']))
+            
+            # VALID
             okValid = controlValid(geom)
             if not okValid:
                 print('%d : Geometry is not valid'%i)
             
-            if 'constraints' in geoFields.keys():
-                if 'bounds' in geoFields['constraints'].keys():
+            if 'constraints' in geoField.keys():
+                # BOUNDS
+                if 'bounds' in geoField['constraints'].keys():
                     bb = geom.bounds
-                    refbb = geoFields['constraints']['bounds']
+                    refbb = geoField['constraints']['bounds']
                     
                     if isinstance(refbb, list):
                     
@@ -267,22 +291,24 @@ def geovalidate(dataPath, schemaPath, geomCol='_geom'):
                         if not okBounds:
                             print('%d : the geometry is not contained by the bounds geometry'%i)
 
-                    
-                if 'minArea' in geoFields['constraints'].keys(): 
-                    okArea = controlArea(geom, data.crs.srs, geoFields['constraints']['minArea'])
+                # MINAREA
+                if 'minArea' in geoField['constraints'].keys(): 
+                    okArea = controlArea(geom, data.crs.srs, geoField['constraints']['minArea'])
                     if not okArea:
-                        print('%d : Area too small. It is smaller than %d square meters'%(i, geoFields['constraints']['minArea']))
-                    
-                if 'unique' in geoFields['constraints'].keys():
-                    if geoFields['constraints']['unique'] :
+                        print('%d : Area too small. It is smaller than %d square meters'%(i, geoField['constraints']['minArea']))
+                
+                # UNIQUE
+                if 'unique' in geoField['constraints'].keys():
+                    if geoField['constraints']['unique'] :
                         for j in range(data.geometry.count()):
                             if j != i:
                                 okUnique = controlUnique(geom, data.geometry[j])
                                 if not okUnique:
                                     print('%d : %d equals %d (duplicates)'%(i,i,j))
                 
-                if 'overlaps' in geoFields['constraints'].keys():
-                    if not geoFields['constraints']['overlaps'] :
+                # OVERLAPS
+                if 'overlaps' in geoField['constraints'].keys():
+                    if not geoField['constraints']['overlaps'] :
                         for j in range(data.geometry.count()):
                             if j != i:
                                 okOverlaps = controlOverlaps(geom, data.geometry[j])
--- geovalidate.bat
+++ geovalidate.bat
@@ -1,27 +1,7 @@
 @echo off
 
-:: POINT ::
-C:\python39\python geovalidate.py invalid-point.csv schema-point.json
-echo ---
+:: see mds/BATCH-EXAMPLES for more examples
 
-
-:: POLYGON ::
-
-rem _geom column
 C:\python39\python geovalidate.py invalid-polygon.csv schema-polygon.json
-echo ---
-
-rem geompol column
-C:\python39\python geovalidate.py invalid-polygon2.csv schema-polygon.json geompol
-echo ---
-
-rem bounds specified as a polygon
-C:\python39\python geovalidate.py invalid-polygon2.csv schema-polygon-with-geombounds.json geompol
-echo ---
-
-
-:: POINT & POLYGON ::
-
-C:\python39\python geovalidate.py invalid-mixed.csv schema-mixed.json
 
 pause
